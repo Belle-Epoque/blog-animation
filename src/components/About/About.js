9c00759785a62307f87cf2ba652a31a435ee754e
Edouard <e.poirson@agence-belle-epoque.fr>
2018-02-28 17:15:43 +0100

    gsap animation

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,24 +1,30 @@
-import React from "react";
-import Transition from "react-transition-group/Transition";
+import React, { Component } from "react";
+import "./About.css";
+import { TimelineLite } from "gsap";
+import RaisedButton from "material-ui/RaisedButton";
 
-const duration = 300;
+class About extends Component {
+  animate() {
+    const animation = new TimelineLite();
+    animation
+      .to(this.box, 1, { x: 100 })
+      .to(this.box, 1, { y: 100 })
+      .to(this.box, 1, { x: 0 })
+      .to(this.box, 1, { y: 0 });
+  }
 
-const defaultStyle = {
-  transition: `opacity ${duration}ms ease-in-out`,
-  opacity: 0
-};
-
-const transitionStyles = {
-  entering: { opacity: 0 },
-  entered: { opacity: 1 }
-};
-
-const About = ({ in: inProp }) => (
-  <Transition timeout={1000}>
-    {status => (
-      <div className={`fade fade-${status}`}>I'm A fade Transition!</div>
-    )}
-  </Transition>
-);
+  render() {
+    return (
+      <div className="container">
+        <RaisedButton
+          onClick={this.animate.bind(this)}
+          primary={true}
+          label="Animate"
+        />
+        <div className="box" ref={box => (this.box = box)} />
+      </div>
+    );
+  }
+}
 
 export default About;

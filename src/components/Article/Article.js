b73e2abbed4253228f5fc061f6d69e0114667c43
lacaux-gerome <gerome.lacaux@hetic.net>
2018-03-02 14:26:07 +0100

    bravo le vo

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,60 +1,73 @@
-import React, { Component, Fragment } from "react";
-import { getArticle } from "../../api/api";
+import React, {Component, Fragment} from "react";
+import {getArticle, getMovie} from "../../api/api";
 import BlackBox from "./BlackBox.js";
 import "./Article.css";
 
 class Article extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      article: {}
-    };
-  }
-
-  componentDidMount() {
-    this.refreshSingleArticle(this.props.match.params.number);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.match.params.number !== this.props.match.params.number) {
-      // Fix bug: force to refresh article state when article id change.
-      this.refreshSingleArticle(nextProps.match.params.number);
-    }
-  }
-
-  async refreshSingleArticle(articleId) {
-    const filterArticle = await getArticle(articleId);
-
-    if (!filterArticle) {
-      // This article doesn't exist.
-      return;
+    constructor(props) {
+        super(props);
+        this.state = {
+            article: {},
+            movie: {}
+        };
     }
 
-    this.setState({
-      article: filterArticle
-    });
-  }
+    componentDidMount() {
+        this.refreshSingleArticle(this.props.match.params.number);
+    }
 
-  render() {
-    const { article: { title, body, img } } = this.state;
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.match.params.number !== this.props.match.params.number) {
+            // Fix bug: force to refresh article state when article id change.
+            this.refreshSingleArticle(nextProps.match.params.number);
+        }
+    }
 
-    return (
-      <Fragment>
-        <div className="Article-img" style={{ backgroundImage: `url(${img})` }}>
-          <BlackBox reverseDirection={false} />
-          <BlackBox reverseDirection={true} />
-          <BlackBox reverseDirection={false} />
-          <BlackBox reverseDirection={true} />
-        </div>
-        <div className="container">
-          <div className="Article-body">
-            <h1 className="Article-title">{title}</h1>
-            <p>{body}</p>
-          </div>
-        </div>
-      </Fragment>
-    );
-  }
+    async refreshSingleArticle(articleId) {
+        console.log('articleId', articleId);
+        const filterMovie = await getMovie(articleId);
+        console.log('filterMovie', filterMovie);
+
+        if (!filterMovie) {
+            // This article doesn't exist.
+            return;
+        }
+
+        this.setState({
+            movie: filterMovie
+        });
+    }
+
+    render() {
+        const {movie: {title, plot, poster}} = this.state;
+        const genres = this.state.movie.genres;
+        if (!genres) return <div>Loading...</div>;
+
+        return (
+            <Fragment>
+                <div className="container">
+                    <div className="Article-body">
+                        <h1 className="Article-title">{title}</h1>
+                        <div>
+                            <img src={poster} alt=""/>
+                        </div>
+                        <p>{plot}</p>
+                        <ul>
+                            {genres.map((genre) => (
+                                <li>{genre}</li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+                <div className="Article-img">
+                    <BlackBox reverseDirection={false}/>
+                    <BlackBox reverseDirection={true}/>
+                    <BlackBox reverseDirection={false}/>
+                    <BlackBox reverseDirection={true}/>
+                </div>
+            </Fragment>
+        );
+    }
 }
 
 export default Article;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,94 +1,107 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import {
-  Card,
-  CardHeader,
-  CardMedia,
-  CardTitle,
-  CardText
+    Card,
+    CardHeader,
+    CardMedia,
+    CardTitle,
+    CardText
 } from "material-ui/Card";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
-import { getArticles, searchMovies, getMovie } from "../../api/api";
-import { Link } from "react-router-dom";
-import { TweenMax } from "gsap";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
+import {getArticles, searchMovies, getMovie} from "../../api/api";
+import {Link} from "react-router-dom";
+import {TweenMax} from "gsap";
 import "./Home.css";
 
-const Fade = ({ children, ...props }) => (
-  <CSSTransition {...props} timeout={3000} classNames="fade">
-    {children}
-  </CSSTransition>
+const Fade = ({children, ...props}) => (
+    <CSSTransition {...props} timeout={3000} classNames="fade">
+        {children}
+    </CSSTransition>
 );
 
 class Home extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      movies: [],
-      show: false
-    };
+        this.state = {
+            movies: []
+        };
 
-    // Tableau de référence des images.
-    this.refImages = [];
-  }
+        // Tableau de référence des images.
+        this.refImages = [];
+    }
 
-  async componentDidMount() {/*
-    const movies = await getMovie();
-    console.log('movies',movies);*/
-    const movies = await searchMovies("deadpool");
-    console.log(movies);
-    const firstFullDataMovie = await getMovie(movies[0].imdb);
-    console.log(firstFullDataMovie);
+    async componentDidMount() {
 
-    this.setState({
-      movies,
-      show: true
-    });
-  }
+        const articles = await getArticles();
 
-  animate(i) {
-    TweenMax.to(this.refImages[i], 2, { opacity: 0 });
-  }
+        const movies = await searchMovies({
+            terms: "The godfather", // Required string
+            //year: 1999, // optional number
+            page: 1 // optional number (1 - 100)
+            //type: "movie" // optional string ("series" || "movie" || "episode")
+        });
+        console.log("DEBUG", movies);
+        const firstFullDataMovie =
+            movies.length > 0 ? await getMovie(movies[0].imdb) : {};
+        console.log(firstFullDataMovie);
 
-  render() {
-    const articles = this.state.articles;
-    const movies = this.state.movies;
-    return (
-      <div className="Home">
-        <div className="Home-intro">
-          <div className="container">
-            <TransitionGroup className="todo-list">
-              {movies.map((movie, i) => (
-                <Fade key={movie.id}>
-                  <div className="Card">
-                    <button onClick={() => this.animate(i)}>Click</button>
-                    <Card>
-                      <Link to={`/article/${movie.id}`} className="Card-link">
-                        <CardHeader
-                          title="Bob"
-                          subtitle="Web dev"
-                          avatar="https://cdn.drawception.com/images/avatars/569903-A55.jpg"
-                        />
-                        <div ref={img => (this.refImages[i] = img)}>
-                          <CardMedia
-                            className="Card-media"
-                            style={{ backgroundImage: `url(${movie.img})` }}
-                            overlay={<CardTitle title={movie.title} />}
-                            overlayContentStyle={{ background: "transparent" }}
-                            overlayStyle={{ color: "#fff" }}
-                          />
+
+        const moviesDetails = await movies.map(async (movie) => (
+            await getMovie(movie.imdb)
+        ));
+        console.log(moviesDetails);
+
+        this.setState({
+            articles,
+            movies,
+            show: true
+        });
+    }
+
+    render() {
+        const articles = this.state.articles;
+        const movies = this.state.movies;
+
+        return (
+            <div className="Home">
+                <div className="Home-intro">
+                    <div className="container">
+                        <div>
+                            <ul>
+                                <button></button>
+                            </ul>
                         </div>
-                        <CardText>{movie.excerpt}</CardText>
-                      </Link>
-                    </Card>
-                  </div>
-                </Fade>
-              ))}
-            </TransitionGroup>
-          </div>
-        </div>
-      </div>
-    );
-  }
+                        <TransitionGroup className="todo-list">
+                            {movies.map((movie, i) => (
+                                <Fade key={movie.imdb}>
+                                    <div className="Card">
+                                        <Card>
+                                            <Link to={`/movie/${movie.imdb}`} className="Card-link">
+                                                <div ref={img => (this.refImages[i] = img)}>
+                                                    <div className="cardInfos">
+                                                        <p>{movie.title}</p>
+                                                        <p>{movie.year.toString()}</p>
+                                                    </div>
+                                                    <CardMedia
+                                                        className="Card-media"
+                                                        style={{backgroundImage: `url(${movie.poster})`}}
+                                                        overlay={<CardTitle title={movie.title}/>}
+                                                        overlayContentStyle={{background: "transparent"}}
+                                                        overlayStyle={{color: "#fff"}}
+                                                    />
+                                                </div>
+                                                <CardText>{movie.excerpt}</CardText>
+                                            </Link>
+                                        </Card>
+                                    </div>
+                                </Fade>
+                            ))}
+                        </TransitionGroup>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Home;

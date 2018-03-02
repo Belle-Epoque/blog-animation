51cda7220f14bc6a9e4720da8c3c8b930f2871d4
Samy Yahia <samy.yahia@hetic.net>
2018-03-02 16:28:02 +0100

    Fix Css

--- src/components/Media/BlackBox.js
+++ src/components/Media/BlackBox.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Motion, spring } from "react-motion";
 
-const BlackBox = ({ reverseDirection = false }) => (
+const BlackBox = ({ reverseDirection = true }) => (
   <Motion defaultStyle={{ scaleX: 1 }} style={{ scaleX: spring(0) }}>
     {interpolatingStyle => (
       <div
         className="black-box"
         style={{
-          transformOrigin: reverseDirection ? `left` : `right`,
+          transformOrigin: reverseDirection ? `top` : `bottom`,
           transition: '0.3s',
           transform: `scaleX(${interpolatingStyle.scaleX})`
         }}
--- src/components/Media/Media.js
+++ src/components/Media/Media.js
@@ -10,8 +10,6 @@ class Media extends Component {
     this.state = {
       media: {}
     };
-
-
   }
 
   componentDidMount() {
@@ -29,9 +27,6 @@ class Media extends Component {
     this.setState({
       media: filterMedia
     });
-
-    console.log(filterMedia);
-
   }
 
   renderAwards = (awards) => {
@@ -48,7 +43,7 @@ class Media extends Component {
   renderArray = (arr) => {
     if (arr) {
       return (arr.map((v, k) => (
-        ` ${v},`
+        <li key={k} >{v}</li>
       )));
     }
   }
@@ -59,101 +54,110 @@ class Media extends Component {
     } = this.state;
 
     return (
-      <div className='Media-single'>
-        <div className='Media-banner' style={{ backgroundImage: `url(${media.poster})` }}>
-          <h2 className='Media-Single-title'>{media.title}</h2>
-        </div>
-        <div className='Media-container'>
-          <div className='Media-aside'>
-            <div className='Media-poster'>
-              <img src={media.poster} alt={media.title}/>
+        <Fragment>
+        <BlackBox />
+        <div className='Media-single'>
+          <div className='Media-banner' style={{ backgroundImage: `url(${media.poster})` }}>
+            <h2 className='Media-Single-title'>{media.title}</h2>
+          </div>
+          <div className='Media-container'>
+            <div className='Media-aside'>
+              <div className='Media-poster'>
+                <img src={media.poster} alt={media.title}/>
+              </div>
+              <div className='Media-awards'>
+                <ul>
+                  <li className='Media-boxOffice'>Box Office: {media.boxOffice}</li>
+                  {this.renderAwards(media.awards)}
+                </ul>
+              </div>
             </div>
-            <div className='Media-awards'>
+            <div className='Media-content'>
+              <div className="Media-Single-info">
+                <ul>
+                  <li>
+                    <span>Countries:</span>
+                    <span>
+                      <ul className='Listing'>
+                        {this.renderArray(media.countries)}
+                      </ul>
+                    </span>
+                  </li>
+                  <li>
+                    <span>Director:</span>
+                    <span>
+                      { (media.director) ? `${media.director}`: `Unknown` }
+                    </span>
+                  </li>
+                  <li>
+                    <span>Actors:</span>
+                    <span>
+                      <ul className='Listing'>
+                        {this.renderArray(media.actors)}
+                      </ul>
+                    </span>
+                  </li>
+                  <li>
+                    <span>Writers:</span>
+                    <span>
+                      <ul className='Listing'>
+                        {this.renderArray(media.writers)}
+                      </ul>
+                    </span>
+                  </li>
+                  <li>
+                    <span>Duration:</span>
+                    <span>
+                      {media.runtime} min
+                    </span>
+                  </li>
+                  <li>
+                    <span>Date:</span>
+                    <span>
+                      { (media.year && (media.year.from || media.year.to)) ? `${media.year.from} - ${media.year.to}`: media.year }
+                    </span>
+                  </li>
+                  <li>
+                    <span>Rated:</span>
+                    <span>
+                      {media.rated}
+                    </span>
+                  </li>
+                </ul>
+              </div>
+              <div className='Media-Single-text'>
+                <h3>Plot:</h3>
+                <p>
+                  {media.plot}
+                </p>
+              </div>
+            </div>
+            <div className='Media-score'>
+              <h3>Scores:</h3>
               <ul>
-                <li className='Media-boxOffice'>Box Office: {media.boxOffice}</li>
-                {this.renderAwards(media.awards)}
+                <li>
+                  <span>Metacritic:</span>
+                  <span>
+                    { (media.metacritic) ? `${media.metacritic}`: `Not Rated Yet` }
+                  </span>
+                </li>
+                <li>
+                  <span>Imdb:</span>
+                  <span>
+                    { (media.imdb && media.imdb.rating ) ? `${media.imdb.rating}`: `Not Rated Yet` }
+                  </span>
+                </li>
+                <li>
+                  <span>Tomato:</span>
+                  <span>
+                    { (media.tomato && media.tomato.score ) ? `${media.tomato.score}`: `Not Rated Yet` }
+                  </span>
+                </li>
               </ul>
             </div>
           </div>
-          <div className='Media-content'>
-            <div className="Media-Single-info">
-              <ul>
-                <li>
-                  <span>Countries:</span>
-                  <span>
-                    {this.renderArray(media.countries)}
-                  </span>
-                </li>
-                <li>
-                  <span>Director:</span>
-                  <span>
-                    { (media.director) ? `${media.director}`: `Unknown` }
-                  </span>
-                </li>
-                <li>
-                  <span>Actors:</span>
-                  <span>
-                    {this.renderArray(media.actors)}
-                  </span>
-                </li>
-                <li>
-                  <span>Writers:</span>
-                  <span>
-                    {this.renderArray(media.writers)}
-                  </span>
-                </li>
-                <li>
-                  <span>Duration:</span>
-                  <span>
-                    {media.runtime} min
-                  </span>
-                </li>
-                <li>
-                  <span>Date:</span>
-                  <span>
-                    { (media.year && (media.year.from || media.year.to)) ? `${media.year.from} - ${media.year.to}`: media.year }
-                  </span>
-                </li>
-                <li>
-                  <span>Rated:</span>
-                  <span>
-                    {media.rated}
-                  </span>
-                </li>
-              </ul>
-            </div>
-            <div className='Media-Single-text'>
-              <h3>Plot:</h3>
-              <p>
-                {media.plot}
-              </p>
-            </div>
-          </div>
-          <div className='Media-score'>
-            <h3>Scores:</h3>
-            <ul>
-              <li>
-                <span>Metacritic:</span>
-                <span>
-                  { (media.metacritic) ? `${media.metacritic}`: `Not Rated Yet` }
-                </span>
-              </li>
-              <li>
-                <span>Imdb:</span>
-                <span>
-                  { (media.imdb && media.imdb.rating ) ? `${media.imdb.rating}`: `Not Rated Yet` }
-                </span>
-              </li>
-              <li>
-                <span>Tomato:</span>
-                <span>
-                  { (media.tomato && media.tomato.score ) ? `${media.tomato.score}`: `Not Rated Yet` }
-                </span>
-              </li>
-            </ul>
-          </div>
         </div>
-      </div>
+      </Fragment>
     );
   }
 }
